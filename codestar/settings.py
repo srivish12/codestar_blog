2d88a3011c90463b0e9771c7265cbe9cde8fc0e9
srivish12 <nalincha30@gmail.com>
2025-09-27 19:11:53 +0100

    sqlite3

--- codestar/settings.py
+++ codestar/settings.py
@@ -78,16 +78,30 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+#DATABASES = {
+ #  'default': {
+  #  'ENGINE': 'django.db.backends.sqlite3',
+   #   'NAME': BASE_DIR / 'db.sqlite3',
+   #}
+#}
+#DATABASES = {
+ #  'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#}
 
-DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if os.environ.get("DEVELOPMENT") == "True":
+    # Use SQLite for development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # Use PostgreSQL for production
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
